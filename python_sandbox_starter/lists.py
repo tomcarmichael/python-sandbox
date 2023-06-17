e9acefd6593e9deac9a6bec455ed46884ee16682
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-06-17 18:22:28 +0100

    lists.py experimentation

--- python_sandbox_starter/lists.py
+++ python_sandbox_starter/lists.py
@@ -1,1 +1,25 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+numbers = [1, 2, 3]
+
+other_list = list((1, 2, 3))
+print(numbers, other_list, type(numbers), type(other_list))
+
+veg = ['aubergine', 'fennel', 'celeriac']
+print(len(veg))
+
+veg.append('potato')
+print(veg)
+
+veg.remove('fennel')
+
+veg.reverse()
+print(veg)
+
+veg.sort(reverse=True)
+print(veg)
+
+copy_veg = veg
+
+veg[1] = 'kale'
+print(copy_veg, copy_veg == veg)
