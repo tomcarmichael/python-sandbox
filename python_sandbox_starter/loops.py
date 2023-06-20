9ce2404df9b8f35205be871c0ad87875eb0d2332
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-06-20 02:03:18 +0100

    loops.py experimentation

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -1,5 +1,9 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+vegetables = ['aubergine', 'fennel', 'celeriac']
 
+for vegetable in vegetables:
+    print(f'{vegetable} is a tasy vegetable.')
 
-# While loops execute a set of statements as long as a condition is true.
+for i in range(0,11):
+    print(f'{i}')
