c804add8909b26641e49e2d9335a5ca7d9934c5b
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-06-20 01:33:06 +0100

    functions.py experimentation

--- python_sandbox_starter/functions.py
+++ python_sandbox_starter/functions.py
@@ -1,7 +1,15 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
+def sayHello(name="Tom"):
+    print(f'Hello {name}')
 
+sayHello()
+
+sayHello('Dave')
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
+get_product = lambda i, j: i * j
+
+print(get_product(3, 5))
