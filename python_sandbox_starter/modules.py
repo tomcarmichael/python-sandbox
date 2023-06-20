f3908dbe4aa8ad8e27b9203693a62a7a5c8ff325
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-06-20 02:11:26 +0100

    modules.py

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,10 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+# import datetime
+# today = datetime.date.today()
+
+from datetime import date
+today = date.today()
+
+
+print(today)
